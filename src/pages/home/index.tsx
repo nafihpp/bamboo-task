d12433cb71f47faeb1fb1ed496a8549558eec77a
Mohammed Nafih Yuseph <nafihpp@gmail.com>
2025-02-18 13:08:01 +0400

    [route]: added proper routing for the application

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomeComponent from "../../components/screens/home";
+
+const HomePage: React.FC = () => {
+  return <HomeComponent />;
+};
+
+export default HomePage;
---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { lazy } from "react";
+import HomePage from "../pages/home";
+const FavouritePage = lazy(() => import("../pages/favourites/index"));
+const Error404 = lazy(() => import("../pages/404"));
+
+const routes = [
+  {
+    path: "/",
+    element: <HomePage />,
+    title: "Home Page",
+  },
+  {
+    path: "/favourites",
+    element: <FavouritePage />,
+    title: "Favourites",
+  },
+  {
+    path: "*",
+    element: <Error404 />,
+    title: "404",
+  },
+];
+
+export default routes;
