c25bd001346249e579aa67c8e5a6e4b71563efee
Mohammed Nafih Yuseph <nafihpp@gmail.com>
2025-02-18 13:08:37 +0400

    [page]: added favourite page

---
+++ src/pages/favourites/index.tsx
@@ -0,0 +1,9 @@
+import FavouriteComponent from "../../components/screens/favourites";
+
+const FavouritePage = () => {
+  return (
+    <FavouriteComponent />
+  );
+};
+
+export default FavouritePage;
